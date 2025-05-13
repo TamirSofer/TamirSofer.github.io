119da0d92a2e280bc3053f4acfd867d45e3f3436
Tamir Sofer <tamirush123123@gmail.com>
2025-05-14 00:24:22 +0300

    Add files via upload

---
+++ SmartAlarmSystem.js
@@ -0,0 +1,186 @@
+'use strict';
+document.getElementById("sendPin").addEventListener("click", function () 
+{
+  const input = document.getElementById('myInput').value;
+  fetch('http://192.168.68.111/send', 
+  {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ type: "pin" ,text: input })
+  })
+    .then(response => response.text())
+    .then(data => {
+      console.log('Success:', data);
+      alert('Sent: ' + data);
+
+      // Clear the input after sending
+      document.getElementById('myInput').value = '';
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      alert('Failed to send');
+    });
+});
+document.getElementById("Left").addEventListener("click", function () 
+{
+  fetch('http://192.168.68.111/send', 
+  {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ type: "ServoLeft" })
+  })
+    .then(response => response.text())
+    .then(data => {
+      console.log('Success:', data);
+      alert('Sent: ' + data);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      alert('Failed to send');
+    });
+});
+document.getElementById("Right").addEventListener("click", function () 
+{
+  fetch('http://192.168.68.111/send', 
+  {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ type: "ServoRight" })
+  })
+    .then(response => response.text())
+    .then(data => {
+      console.log('Success:', data);
+      alert('Sent: ' + data);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      alert('Failed to send');
+    });
+});
+document.getElementById("manualControl").addEventListener("click", function () 
+{
+  fetch('http://192.168.68.111/send', 
+  {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({type: "manual" , text: true })
+  })
+    .then(response => response.text())
+    .then(data => 
+    {
+      console.log('Manual mode activated');
+      // Hide manual button and show auto button
+      document.getElementById("manualControl").style.display = "none";
+      document.getElementById("autoControl").style.display = "inline-block";
+      document.getElementById("Left").style.display = "inline-block";
+      document.getElementById("Right").style.display = "inline-block";
+    })
+    .catch(error => 
+    {
+      console.error('Error:', error);
+      alert('Failed to send');
+    });
+});
+
+document.getElementById("autoControl").addEventListener("click", function () 
+{
+  fetch('http://192.168.68.111/send', 
+  {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({type: "auto" , text: true })
+  })
+    .then(response => response.text())
+    .then(data => 
+    {
+      console.log('Auto mode activated');
+      // Show manual button again
+      document.getElementById("manualControl").style.display = "inline-block";
+      document.getElementById("autoControl").style.display = "none";
+      document.getElementById("Left").style.display = "none";
+      document.getElementById("Right").style.display = "none";
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      alert('Failed to send');
+    });
+});
+document.getElementById("camera").addEventListener("click", function() 
+{
+  window.open("http://192.168.1.100", "_blank"); // open new tab with camera
+});
+function updateBreach() 
+{
+  fetch('http://192.168.68.111/Breach')  //gets the data from the board
+    .then(response => response.text())//makes it a text type
+    .then(data => {
+      // Update the content of the <p> tag with id 'msg' to display the string
+      if(data === "detected")
+      {
+        document.getElementById("cameraControls").style.display = "block";
+      }
+      else
+      {
+        document.getElementById("cameraControls").style.display = "none";
+      }
+      document.getElementById('Breach').textContent = data;
+    })
+    .catch(error => console.error("Error:", error));
+}
+function updatePir1() 
+{
+  fetch('http://192.168.68.111/pir1')  //gets the data from the board
+    .then(response => response.text())//makes it a text type
+    .then(data => {
+      // Update the content of the <p> tag with id 'msg' to display the string
+      document.getElementById('movementSensor1').textContent = data;
+    })
+    .catch(error => console.error("Error:", error));
+}
+function updatePir2() 
+{
+  fetch('http://192.168.68.111/pir2')  //gets the data from the board
+    .then(response => response.text())//makes it a text type
+    .then(data => {
+      // Update the content of the <p> tag with id 'msg' to display the string
+      document.getElementById('movementSensor2').textContent = data;
+    })
+    .catch(error => console.error("Error:", error));
+}
+function updateMic1() 
+{
+  fetch('http://192.168.68.111/mic1')  //gets the data from the board
+    .then(response => response.text())//makes it a text type
+    .then(data => {
+      // Update the content of the <p> tag with id 'msg' to display the string
+      document.getElementById('soundSensor1').textContent = data;
+    })
+    .catch(error => console.error("Error:", error));
+}
+function updateMic2() 
+{
+  fetch('http://192.168.68.111/mic2')  //gets the data from the board
+    .then(response => response.text())//makes it a text type
+    .then(data => {
+      // Update the content of the <p> tag with id 'msg' to display the string
+      document.getElementById('soundSensor2').textContent = data;
+    })
+    .catch(error => console.error("Error:", error));
+}
+
+//update every second
+setInterval(updatePir1, 1000);
+setInterval(updatePir2, 1000);
+setInterval(updateMic1, 1000);
+setInterval(updateMic2, 1000);
+setInterval(updateBreach, 1000);
