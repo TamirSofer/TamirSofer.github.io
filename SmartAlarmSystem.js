db387a9db242ca5e5e31f2432d4cc930c6c6555c
Tamir Sofer <tamirush123123@gmail.com>
2025-05-15 12:01:32 +0300

    Update SmartAlarmSystem.js

--- SmartAlarmSystem.js
+++ SmartAlarmSystem.js
@@ -19,7 +19,7 @@ function getFromESP(endpoint) {
   return fetch(`${apiBase}/${endpoint}`).then(response => response.text());
 }
 
-fetch('https://gist.githubusercontent.com/TamirSofer/17549519d051b4520ff175fce213e002/raw/b50d03946c4d0c5bcb5347e0dfdd7609079aaf0f/ngrok-url.txt')
+fetch('https://gist.githubusercontent.com/TamirSofer/17549519d051b4520ff175fce213e002/raw/ee81b0d4404de4f2b9d57d61cefa28b179ab9f9f/ngrok-url.txt')
   .then(response => response.text())  // get the raw text
   .then(text => {
     console.log("Raw Gist content:", text);
